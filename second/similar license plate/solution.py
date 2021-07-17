0c88304dde35bc7f367c529316a4513695bd5c9f
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-07-17 10:17:53 +0900

    first try

--- second/similar license plate/solution.py
+++ second/similar license plate/solution.py
@@ -1,3 +1,12 @@
+import string
+
+# alphabet lower cases
+SMALL_ALPHA = string.ascii_lowercase
+
+# alphabet upper case
+BIG_ALPHA = string.ascii_uppercase
+
+
 def get_problem():
     # get number of test case
     t = int(input())
@@ -16,10 +25,65 @@ def get_problem():
 
         # value for save license_plates
         license_plates = str(input()).split(' ')
-
         
+        # add result
         parking_lots.append({'k': k, 'parking_lot': license_plates})
 
     return parking_lots
 
-print(get_problem())
+
+def parse_license_plates(license_plates):
+    result = []
+
+
+    for license_plate in license_plates:
+        # value for save result of each liecnse_pate
+        parsed_license_plate = {
+            'license_plate': license_plate, 
+            'small': {}, 
+            'big': 0
+        }
+
+        # low license_plate
+        low_license_plate = license_plate.lower()
+
+
+        for low in SMALL_ALPHA:
+            # add each low and count how many in license_plates
+            parsed_license_plate['small'][low] = low_license_plate.count(low)
+
+        
+        for letter in license_plate:
+            if letter in BIG_ALPHA:
+                parsed_license_plate['big'] += 1
+
+        result.append(parsed_license_plate)
+
+    return result
+
+
+def main():
+    # get problems
+    parking_lots = get_problem()
+
+    result = []
+
+    for parking_lot in parking_lots:
+        parsed_license_plates \
+            = parse_license_plates(parking_lot['parking_lot'])
+        
+        count = 0
+        i = 0
+
+        for i in range(len(parsed_license_plates)):
+            for a in range(i + 1, len(parsed_license_plates)):
+                if (parsed_license_plates[i]['big'] == parsed_license_plates[a]['big']\
+                    and parsed_license_plates[i]['small'] == parsed_license_plates[a]['small']):
+                    count += 1
+
+        result.append(count)
+
+    for a in result:
+        print(a)
+
+main()
